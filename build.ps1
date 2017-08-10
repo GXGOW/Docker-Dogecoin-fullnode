1cb5220ccc24f4bd8c98c461cedf4c1884922d23
Nicolas Loots <nicolas.loots@outlook.be>
2017-08-10 11:55:23 +0200

    Removed a parameter

--- build.ps1
+++ build.ps1
@@ -4,4 +4,4 @@ docker stop litenode
 docker rm litenode
 docker rmi liteimage
 docker build -t='liteimage' .
-docker run -td -p 9333:9333 --name litenode liteimage
+docker run -d -p 9333:9333 --name litenode liteimage
